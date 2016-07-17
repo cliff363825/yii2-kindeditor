5b1358e92c0f20b06bea865f2e194ba52d5eee35
fage <fage@juanpi.com>
2016-07-17 16:57:39 +0800

    fix asset

--- KindEditorAsset.php
+++ KindEditorAsset.php
@@ -10,21 +10,10 @@ use yii\web\AssetBundle;
 class KindEditorAsset extends AssetBundle
 {
     public $sourcePath = '@cliff363825/kindeditor/assets';
-    public $js = [];
+    public $js = [
+        YII_ENV_DEV ? 'kindeditor-all.js' : 'kindeditor-all-min.js',
+    ];
     public $css = [
         'themes/default/default.css',
     ];
-
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        parent::init();
-        if (YII_DEBUG) {
-            $this->js[] = 'kindeditor-all.js';
-        } else {
-            $this->js[] = 'kindeditor-all-min.js';
-        }
-    }
 }
