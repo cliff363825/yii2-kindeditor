6421be90fc579f75d9d69c6ece5edaa92cd9cf33
fage <fage@juanpi.com>
2016-07-27 16:25:58 +0800

    Revert "fix asset"
    This reverts commit 5b1358e92c0f20b06bea865f2e194ba52d5eee35.

--- KindEditorAsset.php
+++ KindEditorAsset.php
@@ -10,10 +10,21 @@ use yii\web\AssetBundle;
 class KindEditorAsset extends AssetBundle
 {
     public $sourcePath = '@cliff363825/kindeditor/assets';
-    public $js = [
-        YII_ENV_DEV ? 'kindeditor-all.js' : 'kindeditor-all-min.js',
-    ];
+    public $js = [];
     public $css = [
         'themes/default/default.css',
     ];
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        if (YII_DEBUG) {
+            $this->js[] = 'kindeditor-all.js';
+        } else {
+            $this->js[] = 'kindeditor-all-min.js';
+        }
+    }
 }
